c661710f9d7622af72f8c1993cb1b9bbf0cd4c70
Mohamed Magdy <moh.magdy40@gmail.com>
2015-04-17 21:13:17 +0200

    Customiztion to hr.employee: ** Add a new field 'Employee Code': Unique, Integer and Required. ** Employee Code is added to views (Tree, Form and Kanban) and is the first search pattern. ** Home Address is now Required.

--- hr_employee_code/hr_employee_code.py
+++ hr_employee_code/hr_employee_code.py
@@ -1,1 +1,14 @@
+from openerp.osv import fields, osv
 
+
+class hr_employee(osv.Model):
+    _name = 'hr.employee'
+    _inherit = 'hr.employee'
+
+    _columns = {
+        'employee_code': fields.integer(string="Employee Code", required=True, ),
+    }
+
+    _sql_constraints = [
+        ('employee_code_uniq', 'unique(employee_code)', 'The Code of the Employee must be unique!'),
+    ]
