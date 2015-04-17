080434ca48cc713bee51cf38fb8eb39019b9a84e
Mohamed Magdy <moh.magdy40@gmail.com>
2015-04-17 20:21:40 +0200

    Add hr_events module (Basic Structure)

---
+++ hr_events/__openerp__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'HR Events',
+    'version': '0.1',
+    'author': 'Cubex Solutions',
+    'website': 'http://www.cubex.solutions/',
+    'category': 'Human Resources',
+    'summary': """
+    Simple Customization to fit a specific needs.
+    """,
+    'depends': ['hr', 'event'],
+    'data':['hr_events_view.xml', ],
+    'installable': True,
+    'auto_install': False,
+}
